1df786f375c5985091b18069239919fdb70cb336
unknown <julio.acc@hotmail.com>
2025-09-18 23:12:06 -0300

    feat: adiciona rota PUT

--- resources/book.py
+++ resources/book.py
@@ -29,7 +29,6 @@ class Book(Resource):
 
     def post(self, book_id):
         argumentos = reqparse.RequestParser()
-
         argumentos.add_argument('title')
         argumentos.add_argument('author')
         argumentos.add_argument('year')
@@ -47,7 +46,31 @@ class Book(Resource):
         return new_book, 200
 
     def put(self, book_id):
-        pass
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('title')
+        argumentos.add_argument('author')
+        argumentos.add_argument('year')
+
+        dados = argumentos.parse_args()
+
+        new_book = {
+            'book_id': book_id,
+            'title': dados['title'],
+            'author': dados['author'],
+            'year': dados['year']
+        }
+
+        for book in books:
+            if books['book_id'] == book_id:
+                return book
+        return None
+        if book is not None:
+            for book in books:
+                if book['book_id'] == book_id:
+                    book.update(new_book)
+                    return new_book, 200
+                books.append(book)
+                return new_book, 201
 
     def delete(self, book_id):
         pass
