738a9d37ce96d767c1c093715191dc86387f6f37
unknown <julio.acc@hotmail.com>
2025-09-18 21:58:00 -0300

    feat: adiciona rota POST

--- resources/book.py
+++ resources/book.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 books = [
     {
@@ -28,7 +28,23 @@ class Book(Resource):
         return {'message': 'Book not found'}, 404
 
     def post(self, book_id):
-        pass
+        argumentos = reqparse.RequestParser()
+
+        argumentos.add_argument('title')
+        argumentos.add_argument('author')
+        argumentos.add_argument('year')
+
+        dados = argumentos.parse_args()
+
+        new_book = {
+            'book_id': book_id,
+            'title': dados['title'],
+            'author': dados['author'],
+            'year': dados['year']
+        }
+
+        books.append(new_book)
+        return new_book, 200
 
     def put(self, book_id):
         pass
