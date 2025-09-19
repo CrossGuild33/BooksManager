4a7cdded10075778609810684aecffbcda14c211
unknown <julio.acc@hotmail.com>
2025-09-18 21:29:34 -0300

    feat: adiociona rota GET /books/<string:book_id

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from resources.book import Books
+from resources.book import Books, Book
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,12 +9,8 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-
-
-
-
 api.add_resource(Books, '/books')
+api.add_resource(Book, '/books/<string:book_id>')
 
 # Para criar o banco e rodar a aplicação
 if __name__ == '__main__':
--- resources/book.py
+++ resources/book.py
@@ -5,11 +5,33 @@ books = [
         'title': 'Hora e a Vez de Augusto Matraga',
         'author': 'Guimarães Rosa',
         'year': 1963
+    },
+    {
+        'title': 'O Tempo e o Vento',
+        'author': 'Erico Verissimo',
+        'year': 1952
     }
+
 ]
 
 
 class Books(Resource):
     @staticmethod
-    def get():
-        return {'Books': books}
+    def get(self):
+        return {'books': books}
+
+class Book(Resource):
+    def get(self, book_id):
+        for book in books:
+            if books['book_id'] == book_id:
+                return book
+        return {'message': 'Book not found'}, 404
+
+    def post(self, book_id):
+        pass
+
+    def put(self, book_id):
+        pass
+
+    def delete(self, book_id):
+        pass
