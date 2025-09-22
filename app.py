2fa0eae1ef27a7ab888e2a5a3f26f6f2c29ef777
unknown <julio.acc@hotmail.com>
2025-09-22 19:18:18 -0300

    feat: criação de banco automatico, usando SQLAlchemy

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 from resources.book import Books, Book
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+@app.before_request
+def create_database():
+    database.create_all()
 
 api.add_resource(Books, '/books')
 api.add_resource(Book, '/books/<string:book_id>')
 
 # Para criar o banco e rodar a aplicação
 if __name__ == '__main__':
+    from sql_alchemy import database
+    database.init_app(app)
     app.run(debug=True)
--- models/book.py
+++ models/book.py
@@ -1,4 +1,13 @@
+from sql_alchemy import database
+
 class BookModel:
+    __tablename__ = 'books'
+
+    book_id = database.Column(database.Integer, primary_key=True)
+    title = database.Column(database.String(120))
+    author = database.Column(database.String(100))
+    year = database.Column(database.Integer)
+
     def __init__(self, book_id, title, author, year):
         self.book_id = book_id
         self.title = title
