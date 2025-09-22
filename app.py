4c0db9ec1eb1c2eb51719290f78046dcf39a5baf
unknown <julio.acc@hotmail.com>
2025-09-21 22:49:06 -0300

    refactor: retirando códigos sem uso

--- app.py
+++ app.py
@@ -6,14 +6,9 @@ from resources.book import Books, Book
 app = Flask(__name__)
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 api.add_resource(Books, '/books')
 api.add_resource(Book, '/books/<string:book_id>')
 
 # Para criar o banco e rodar a aplicação
 if __name__ == '__main__':
-    #with app.app_context():
-      #  db.create_all() # Create database in the first run
     app.run(debug=True)
